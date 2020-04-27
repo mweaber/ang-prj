fbeed7ba82f2eeca56bf82f0d67ce3b6bb00d4b8
Matthew Weaber <matthewweaber89@gmail.com>
2020-04-27 12:17:38 -0400

    Adding more components/styles

--- classic-car/src/app/app.module.ts
+++ classic-car/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './car/car.component';
 import { CarsComponent } from './cars/cars.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
-    CarsComponent
+    CarsComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
